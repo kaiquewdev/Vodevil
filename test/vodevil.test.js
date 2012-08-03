c7fcbbc63ef08c96173fc62a77762a591f2ae317
Kaique da Silva <kaique.developer@gmail.com>
2012-08-03 04:56:57 -0300

    Correct name

--- test/vodevil.test.js
+++ test/vodevil.test.js
@@ -3,32 +3,32 @@ var chai = require('chai'),
     should = chai.should(),
     expect = chai.expect,
     sinon = require('sinon'),
-    Voudevil = require('../lib/voudevil').core;
+    Vodevil = require('../lib/vodevil').core;
 
 // Vouvdevil lib main tests
-describe('Voudevil Section of Tests: ', function () {
+describe('Vodevil Section of Tests: ', function () {
     describe('Array number ranges', function () {
         it(' - Simple number range, with string by argument', function () {
             var output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
             // Array aspect
-            Voudevil.range('1..10').toString().should.equal( output.toString() );
+            Vodevil.range('1..10').toString().should.equal( output.toString() );
             // Array length
-            Voudevil.range('1..10').should.have.length( 10 );
+            Vodevil.range('1..10').should.have.length( 10 );
         });
 
         it(' - Simple number range, with separated arguments', function () {
             var output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
             // Array aspect
-            Voudevil.range(1, 10).toString().should.equal( output.toString() );
+            Vodevil.range(1, 10).toString().should.equal( output.toString() );
             // Array length
-            Voudevil.range('1..10').should.have.length( 10 );
+            Vodevil.range('1..10').should.have.length( 10 );
 
         });
 
         it(' - Fail if not receive argument', function () {
-            Voudevil.range().should.have.length( 0 );    
+            Vodevil.range().should.have.length( 0 );    
         });
     });    
 
@@ -36,7 +36,7 @@ describe('Voudevil Section of Tests: ', function () {
         it (' - Simple letter sequence', function () {
             var output = ['a', 'b', 'c'];
 
-            Voudevil.range('a..c').should.have.length( 3 );
+            Vodevil.range('a..c').should.have.length( 3 );
         });
     });
 });
