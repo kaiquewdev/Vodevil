147f0c4add261cf2069a19c7ae890486bacc7980
Kaique da Silva <kaique.developer@gmail.com>
2012-08-03 02:33:34 -0300

    Range
    Returning a list of number or letter

--- voudevil-lib-test.js
+++ voudevil-lib-test.js
@@ -6,22 +6,37 @@ var chai = require('chai'),
     Voudevil = require('./voudevil').core;
 
 // Vouvdevil lib main tests
-describe('Voudevil Section of Tests', function () {
-    describe('Array ranges', function () {
-        it('Simple number range, with string by argument', function () {
+describe('Voudevil Section of Tests: ', function () {
+    describe('Array number ranges', function () {
+        it(' - Simple number range, with string by argument', function () {
             var output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-            Voudevil.range('1..10').should.equal( output );   
+            // Array aspect
+            Voudevil.range('1..10').toString().should.equal( output.toString() );
+            // Array length
+            Voudevil.range('1..10').should.have.length( 10 );
         });
 
-        it('Simple number rang, with separated arguments', function () {
+        it(' - Simple number range, with separated arguments', function () {
             var output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-            Voudevil.range(1, 10).should.equal( output );
+            // Array aspect
+            Voudevil.range(1, 10).toString().should.equal( output.toString() );
+            // Array length
+            Voudevil.range('1..10').should.have.length( 10 );
+
         });
 
-        it('Fail if not receive argument', function () {
-            Voudevil.range().should.equal( [] );    
+        it(' - Fail if not receive argument', function () {
+            Voudevil.range().should.have.length( 0 );    
         });
     });    
+
+    describe('Array letter ranges', function () {
+        it (' - Simple letter sequence', function () {
+            var output = ['a', 'b', 'c'];
+
+            Voudevil.range('a..c').should.have.length( 3 );
+        });
+    });
 });
--- voudevil.js
+++ voudevil.js
@@ -2,7 +2,62 @@
 var Voudevil = (function () {
     var core = function () {};
 
-    core.protytpe = {};
+    // Internal engine functions
+    var __range__ = function ( left, right, jump ) {
+        // create a sequence of array
+        var output = [],
+            jump = jump || 1;
+        
+        for (var s = left; s <= right; s += jump ) {
+            output.push( s );
+        }
+
+        return output;
+    };
+
+    var __letter__ = function ( left, right ) {
+        var output = [],
+            alpha = {};
+            // Alphabetic
+            alpha['upper'] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            alpha['lower'] = alpha['upper'].toLowerCase();
+            alpha['betic'] = alpha.lower + alpha.upper;
+
+        for ( var s = alpha.betic.indexOf( left ), e = alpha.betic.indexOf( right ); s <= e; s++ ) {
+            output.push( alpha.betic[ s ] );
+        }
+
+        return output;
+    };
+
+    core.prototype = {
+        range: function ( left, right ) {
+            var output = [];
+
+            if ( typeof left === 'number' && typeof right === 'number' ) {
+                output = __range__( left, right );
+            } else if ( typeof left === 'string' && !right ) {
+                var pointer = left.split('..');
+
+                if ( 
+                    pointer[0].search(/[0-9]/) > -1 && 
+                    pointer[1].search(/[0-9]/) > -1 
+                ) {
+                    output = __range__( 
+                        parseInt( pointer[0], 10 ), 
+                        parseInt( pointer[1], 10 ) 
+                    );
+                } else if ( 
+                    pointer[0].search(/[a-z]/) > -1 &&
+                    pointer[1].search(/[a-z]/) > -1
+                ) {
+                    output = __letter__( pointer[0], pointer[1] );    
+                }
+            }
+
+            return output;
+        }    
+    };
 
     return new core;
 } ());
